3bfc155100270bbe2d87c887408c0c2c0c479257
Fahmidur-arnob <arnobdev93@gmail.com>
2022-10-18 22:27:09 +0600

    Created login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,8 +3,20 @@ import './Login.css';
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className='form-container'>
+            <h1 className='form-title'>Login Now!</h1>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" required />
+                </div>
+
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" required />
+                </div>
+                <input className='btn-submit' type="submit" value="login" />
+            </form>
         </div>
     );
 };
