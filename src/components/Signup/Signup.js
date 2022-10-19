ffeacea72dcdfd56722dd5c50d2b5ef27b0abb57
Fahmidur-arnob <arnobdev93@gmail.com>
2022-10-19 11:27:08 +0600

    Added getAuth

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Signup.css';
 
 const Signup = () => {
+
+    const [error, setError] = useState(null);
+
+    const handleSubmit = event =>{
+        event.preventDefault();
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirm = form.confirm.value;
+
+        if(password.length < 6){
+            setError(`Password should contain more than 6 characters.`);
+            return;//shamne jaba na return kore jaba;
+        }
+        else if(password !== confirm){
+            setError(`Your password didn't match`);
+            return;//shamne jaba na return kore jaba;
+        }
+    }
+
+
     return (
         <div className='form-container'>
             <h1 className='form-title'>SignUp Now!</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-control">
                     <label htmlFor="email">Email</label>
                     <input type="email" name="email" required />
@@ -22,7 +45,10 @@ const Signup = () => {
                 </div>
 
                 <input className='btn-submit' type="submit" value="sign up" />
-                <p>Already have and account? Go To <Link to="/login">LOGIN</Link> </p>
+
+                <p className='alreadyHave'>Already have and account? Go To <Link className='alreadeHaveLink' to="/login">LOGIN</Link> </p>
+
+                <p className='text-error'>{error}</p>
             </form>
         </div>
     );
--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,12 +1,17 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+import {getAuth} from 'firebase/auth';
+import app from '../firebase/firebase.config';
 
 
 export const AuthContext = createContext();
 
+const auth = getAuth(app);
+
 
 const UserContext = ({children}) => {
-
-    const authInfo = {}
+    const [user, setUser] = useState(null);
+    const authInfo = {user}
+    
 
 
     return (
